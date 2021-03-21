284d72d3ee9e87efbfaabf31f1afff0fe46b2b1f
MarvatheLarva <mtlarva@gmail.com>
2021-03-21 22:30:04 +0100

    -

--- teleportMe/_resource.lua
+++ teleportMe/_resource.lua
@@ -2,9 +2,9 @@
 -- Altering or recreating for local use only is strongly discouraged.
 
 version '1.0.0'
-author 'Cfx.re <root@cfx.re>'
-description 'Handles spawning a player in a unified fashion to prevent resources from having to implement custom spawn logic.'
-repository 'https://github.com/citizenfx/cfx-server-data'
+author 'No one'
+description 'no need'
+repository 'https://github.com/MarvatheLarva/Mevif'
 
 client_script 'script.js'
 
